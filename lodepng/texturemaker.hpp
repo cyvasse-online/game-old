01a59a8bb5281e468622971cf2f46d22fd0e22fd
Jonas Platte <mail@jonasplatte.de>
2015-02-16 04:26:28 +0100

    Fixed indentation in lodepng/texturemaker.hpp

--- lodepng/texturemaker.hpp
+++ lodepng/texturemaker.hpp
@@ -1,9 +1,9 @@
 /* Copyright 2014 Jonas Platte
-*
-* This file is NO part of Cyvasse Online.
-*
-* Use it however you want to.
-*/
+ *
+ * This file is NO part of Cyvasse Online.
+ *
+ * Use it however you want to.
+ */
 
 #ifndef _TEXTUREMAKER_HPP_
 #define _TEXTUREMAKER_HPP_
@@ -17,25 +17,25 @@
 
 inline std::pair<fea::Texture, glm::uvec2> makeTexture(std::string path)
 {
-    unsigned width, height;
+	unsigned width, height;
 
-    std::vector<unsigned char> image; //the raw pixels
+	std::vector<unsigned char> image; //the raw pixels
 
-    // decode
-    unsigned error = lodepng::decode(image, width, height, path);
+	// decode
+	unsigned error = lodepng::decode(image, width, height, path);
 
-    // if there's an error, display it
-    if(error)
-        throw std::runtime_error("(lodepng) error loading " + path + ": " + lodepng_error_text(error));
+	// if there's an error, display it
+	if(error)
+		throw std::runtime_error("(lodepng) error loading " + path + ": " + lodepng_error_text(error));
 
-    fea::Texture texture;
-    texture.create(width, height, &image[0]);
+	fea::Texture texture;
+	texture.create(width, height, &image[0]);
 
-    return std::pair<fea::Texture, glm::uvec2>(
-        std::piecewise_construct,
-        std::forward_as_tuple(std::move(texture)),
-        std::forward_as_tuple(width, height)
-    );
+	return std::pair<fea::Texture, glm::uvec2>(
+		std::piecewise_construct,
+		std::forward_as_tuple(std::move(texture)),
+		std::forward_as_tuple(width, height)
+	);
 }
 
 #endif // _TEXTUREMAKER_HPP_
